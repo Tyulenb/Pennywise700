48605f8492a731329ced4eb2b8815c77e0de0dda
Igor <igoryanec23@gmail.com>
2025-11-16 00:07:40 +0300

    Rewrite hardcoded decoding in each stage to methods in pipeline
    Removed ad-hoc hardware optimisations to write general ones

--- emu/pipeline/pipeline.go
+++ emu/pipeline/pipeline.go
@@ -2,6 +2,7 @@ package pipeline
 
 import (
 	"fmt"
+	"strconv"
 	"sync"
 )
 
@@ -42,4 +43,99 @@ func (p *Pipeline) Move(cmd uint32) {
     }
     p.Pipe[0] = cmd
     p.Alu[0] = ALU{}
+}
+
+func (p *Pipeline) FetchOpcode(stage int) uint8 {
+    return uint8(p.Pipe[stage] & 0x00F00000 >> 20)
+}
+
+func (p *Pipeline) DecodeAdrR1(stage int) uint16 {
+    return uint16(p.Pipe[stage] & 0x000F0000 >> 16)
+}
+
+func (p *Pipeline) DecodeAdrR2(stage int) uint16 {
+    return uint16(p.Pipe[stage] & 0x0000F000 >> 12)
+}
+
+func (p *Pipeline) DecodeAdrR3(stage int) uint16 {
+    return uint16(p.Pipe[stage] & 0x00000F00 >> 8)
+}
+
+func (p *Pipeline) DecodeAdrM(stage int) uint16 {
+    return uint16(p.Pipe[stage] & 0x000003FF)
+}
+
+func (p *Pipeline) DecodeLiteral(stage int) uint16 {
+    return uint16(p.Pipe[stage] & 0x000FFC00 >> 10) 
+}
+
+func (p *Pipeline) DecodeAdrToJump(stage int) uint16 {
+    return p.DecodeAdrM(stage) 
+}
+
+func (p *Pipeline) DropPipe() {
+    for i := range 4 {
+        p.Pipe[i] = 0
+    }
+}
+
+func (p *Pipeline) PipeToString() [5]string {
+    result := [5]string{}
+    for i := range p.Pipe {
+        result[i] = commandToString(p.Pipe[i])
+    }
+    return result 
+}
+
+// Public func to convert commands
+func (p *Pipeline) CommandToString(stage int) string {
+    return commandToString(p.Pipe[stage])
+}
+
+// Private func to convert commands
+func commandToString(cmd uint32) string {
+    opcode := cmd & 0x00F00000 >> 20
+    switch opcode {
+    case 0:
+        return "NOP"
+    case 1:
+        lit := cmd & 0x000FFC00 >> 10
+        adr_m := cmd & 0x000003FF 
+        return fmt.Sprintf("LTM %v %v", strconv.Itoa(int(lit)), strconv.Itoa(int(adr_m)))
+    case 2:
+        adr_r := cmd & 0x000F0000>>16 
+        adr_m := cmd & 0x000003FF 
+        return fmt.Sprintf("MTR %v %v", strconv.Itoa(int(adr_r)), strconv.Itoa(int(adr_m)))
+    case 3:
+        adr_r1 := cmd & 0x000F0000>>16 
+        adr_r2 := cmd & 0x0000F000>>12 
+        return fmt.Sprintf("RTR %v %v", strconv.Itoa(int(adr_r1)), strconv.Itoa(int(adr_r2)))
+    case 4:
+        adr_r1 := cmd & 0x000F0000>>16 
+        adr_r2 := cmd & 0x0000F000>>12 
+        adr_r3 := cmd & 0x00000F00>>8
+        return fmt.Sprintf("SUB %v %v %v", strconv.Itoa(int(adr_r1)), strconv.Itoa(int(adr_r2)), strconv.Itoa(int(adr_r3)))
+    case 5:
+        adr_r1 := cmd & 0x000F0000>>16 
+        adr_r2 := cmd & 0x0000F000>>12 
+        adr_m := cmd & 0x000003FF
+        return fmt.Sprintf("SUB %v %v %v", strconv.Itoa(int(adr_r1)), strconv.Itoa(int(adr_r2)), strconv.Itoa(int(adr_m)))
+    case 6:
+        adr_r1 := cmd & 0x000F0000>>16 
+        adr_r2 := cmd & 0x0000F000>>12 
+        return fmt.Sprintf("MTRK %v %v", strconv.Itoa(int(adr_r1)), strconv.Itoa(int(adr_r2)))
+    case 7:
+        adr_r1 := cmd & 0x000F0000>>16 
+        adr_r2 := cmd & 0x0000F000>>12 
+        return fmt.Sprintf("RTMK %v %v", strconv.Itoa(int(adr_r1)), strconv.Itoa(int(adr_r2)))
+    case 8:
+        adr_to_jmp := cmd & 0x000003FF
+        return fmt.Sprintf("JMP %v", adr_to_jmp)
+    case 9:
+        adr_r1 := cmd & 0x000F0000>>16 
+        adr_r2 := cmd & 0x0000F000>>12 
+        adr_r3 := cmd & 0x00000F00>>8
+        return fmt.Sprintf("SUM %v %v %v", strconv.Itoa(int(adr_r1)), strconv.Itoa(int(adr_r2)), strconv.Itoa(int(adr_r3)))
+    }
+    return ""
 }
